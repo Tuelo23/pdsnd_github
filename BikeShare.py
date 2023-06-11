d77841291452c4ee801eefc3832bd724a44c6dca
Tuelo Setshedi <Tuelo.Setshedi@debeersgroup.com>
2023-06-11 17:29:45 +0200

    Changes Made to the document

---
+++ BikeShare.py
@@ -0,0 +1,198 @@
+import time
+import pandas as pd
+import numpy as np
+
+CITY_DATA = { 'chicago': 'chicago.csv',
+              'new york city': 'new_york_city.csv',
+              'washington': 'washington.csv' }
+
+def get_filters():
+    """
+    Asks user to specify a city, month, and day to analyze.
+
+    Returns:
+        (str) city - name of the city to analyze
+        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    """
+    print('Hello! Let\'s explore some US bikeshare data!')
+    # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    while True:
+        city = input("Enter city name (Chicago, New York City, Washington): ").lower()
+        if city not in CITY_DATA:
+            print("Invalid city name. Please try again.")
+        else:
+            break
+
+    # TO DO: get user input for month (all, january, february, ... , june)
+    while True:
+        month = input("Enter month name (e.g. January, February, March...June) or 'all' for no filter: ").lower()
+        if month not in ['january', 'february', 'march', 'april', 'may', 'june', 'all']:
+            print("Invalid month name. Please try again.")
+        else:
+            break
+
+    # TO DO: get user input for day of week (all, monday, tuesday, ... sunday)
+    while True:
+        day = input("Enter day of the week (e.g. Monday, Tuesday...Sunday) or 'all' for no filter: ").lower()
+        if day not in ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday', 'all']:
+            print("Invalid day of the week. Please try again.")
+        else:
+            break
+
+    print('-'*40)
+    return city, month, day
+
+
+def load_data(city, month, day):
+    """
+    Loads data for the specified city and filters by month and day if applicable.
+
+    Args:
+        (str) city - name of the city to analyze
+        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    Returns:
+        df - Pandas DataFrame containing city data filtered by month and day
+    """
+    df = pd.read_csv(CITY_DATA[city])
+    df['Start Time'] = pd.to_datetime(df['Start Time'])
+    df['month'] = df['Start Time'].dt.month
+    df['day_of_week'] = df['Start Time'].dt.dayofweek
+    df['hour'] = df['Start Time'].dt.hour
+
+    if month != 'all':
+        months = ['january', 'february', 'march', 'april', 'may', 'june']
+        month = months.index(month) + 1
+        df = df[df['month'] == month]
+
+    if day != 'all':
+        days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+        day = days.index(day)
+        df = df[df['day_of_week'] == day]
+
+    return df
+
+
+def time_stats(df):
+    """Displays statistics on the most frequent times of travel."""
+
+    print('\nCalculating The Most Frequent Times of Travel...\n')
+    start_time = time.time()
+
+    # TO DO: display the most common month
+    popular_month = df['month'].mode()[0]
+    months = ['January', 'February', 'March', 'April', 'May', 'June']
+    print('Most Common Month:', months[popular_month - 1])
+
+    # TO DO: display the most common day of week
+    popular_day = df['day_of_week'].mode()[0]
+    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    print('Most Common Day of Week:', days[popular_day])
+
+    # TO DO: display the most common start hour
+    popular_hour = df['hour'].mode()[0]
+    print('Most Common Start Hour:', popular_hour)
+
+    print("\nThis took %s seconds." % (time.time() - start_time))
+    print('-'*40)
+
+
+def station_stats(df):
+    """Displays statistics on the most popular stations and trip."""
+
+    print('\nCalculating The Most Popular Stations and Trip...\n')
+    start_time = time.time()
+
+    # TO DO: display most commonly used start station
+    popular_start_station = df['Start Station'].mode()[0]
+    print('Most Popular Start Station:', popular_start_station)
+
+    # TO DO: display most commonly used end station
+    popular_end_station = df['End Station'].mode()[0]
+    print('Most Popular End Station:', popular_end_station)
+
+    # TO DO: display most frequent combination of start station and end station trip
+    df['Start End Stations'] = df['Start Station'] + ' to ' + df['End Station']
+    popular_start_end_station = df['Start End Stations'].mode()[0]
+    print('Most Popular Trip:', popular_start_end_station)
+
+    print("\nThis took %s seconds." % (time.time() - start_time))
+    print('-'*40)
+
+
+def trip_duration_stats(df):
+    """Displays statistics on the total and average trip duration."""
+       
+    print('\nCalculating Trip Duration...\n')
+    start_time = time.time()
+
+    # TO DO: display total travel time
+    total_travel_time = df['Trip Duration'].sum()
+    print('Total Travel Time (seconds):', total_travel_time)
+
+    # TO DO: display mean travel time
+    mean_travel_time = df['Trip Duration'].mean()
+    print('Mean Travel Time (seconds):', mean_travel_time) 
+
+    print("\nThis took %s seconds." % (time.time() - start_time))
+    print('-'*40)
+
+
+def user_stats(df):
+    """Displays statistics on bikeshare users."""
+
+    print('\nCalculating User Stats...\n')
+    start_time = time.time()
+
+    # TO DO: Display counts of user types
+    user_counts = df['User Type'].value_counts()
+    print('Counts of User Types:\n', user_counts)
+
+    # TO DO: Display counts of gender
+    if 'Gender' in df.columns:
+        gender_counts = df['Gender'].value_counts()
+        print('Counts of Gender:\n', gender_counts)    
+
+    # TO DO: Display earliest, most recent, and most common year of birth
+    if 'Birth Year' in df.columns:
+        earliest_birth_year = df['Birth Year'].min()
+        print('Earliest Birth Year:', int(earliest_birth_year))
+        most_recent_birth_year = df['Birth Year'].max()
+        print('Most Recent Birth Year:', int(most_recent_birth_year))
+        most_common_birth_year = df['Birth Year'].mode()[0]
+        print('Most Common Birth Year:', int(most_common_birth_year))
+    else:
+        print('Birth year information is not available for this dataset.')
+
+    print("\nThis took %s seconds." % (time.time() - start_time))
+    print('-'*40)
+ 
+def display_data(df):
+    """Displays rows of data based on user's request."""
+
+    start_loc = 0
+    view_display = input("Do you want to see the first 10 rows of data? Enter yes or no: ").lower()
+
+    while view_display == 'yes':
+        print(df.iloc[start_loc:start_loc+10])
+        start_loc += 10
+        view_display = input("Do you want to see the next 10 rows of data? Enter yes or no: ").lower()
+
+def main():
+    while True:
+        city, month, day = get_filters()
+        df = load_data(city, month, day)
+
+        time_stats(df)
+        station_stats(df)
+        trip_duration_stats(df)
+        user_stats(df)
+
+        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        if restart.lower() != 'yes':
+            break
+
+
+if __name__ == "__main__":
+	main()
--- README.md
+++ README.md
@@ -1,17 +1,17 @@
->**Note**: Please **fork** the current Udacity repository so that you will have a **remote** repository in **your** Github account. Clone the remote repository to your local machine. Later, as a part of the project "Post your Work on Github", you will push your proposed changes to the remote repository in your Github account.
+**Note**: Please **fork** the current Udacity repository so that you will have a **remote** repository in **your** Github account. Clone the remote repository to your local machine. Later, as a part of the project "Post your Work on Github", you will push your proposed changes to the remote repository in your Github account.
 
-### Date created
+### 2023 June 11
 Include the date you created this project and README file.
 
-### Project Title
+### BikeShare
 Replace the Project Title
 
-### Description
+### The project is about BikeShare performance(traveling times) through 3 cities Chicago, New York and Washington
 Describe what your project is about and what it does
 
-### Files used
+### BikeShare.py
 Include the files used
 
-### Credits
+### https://github.com/udacity/pdsnd_github
 It's important to give proper credit. Add links to any repo that inspired you or blogposts you consulted.
 
